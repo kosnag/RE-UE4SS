4de7d9ded18df1bd19f8a210b773883a4e62fd94
kosnag <63012171+kosnag@users.noreply.github.com>
2024-02-27 23:05:52 +0300

    Update LuaMod.cpp
    Fixed not working BPModLoader for Ace Combat 7

--- UE4SS/src/Mod/LuaMod.cpp
+++ UE4SS/src/Mod/LuaMod.cpp
@@ -2120,7 +2120,7 @@ Overloads:
 #1: IterateGameDirectories()"};
 
             std::filesystem::path module_directory = UE4SSProgram::get_program().get_module_directory();
-            auto game_content_dir = module_directory.parent_path().parent_path().parent_path() / "Content";
+            auto game_content_dir = module_directory / "Content";
             if (!std::filesystem::exists(game_content_dir))
             {
                 Output::send<LogLevel::Warning>(STR("IterateGameDirectories: Could not locate the root directory because the directory structure is unknown "
@@ -2129,8 +2129,9 @@ Overloads:
                 return 1;
             }
 
-            auto game_name = module_directory.parent_path().parent_path().parent_path().stem();
-            auto game_root_directory = module_directory.parent_path().parent_path().parent_path().parent_path();
+            //auto game_name = module_directory.parent_path().parent_path().parent_path().stem();
+            auto game_name = "Ace7Game";
+            auto game_root_directory = module_directory;
             auto directories_table = lua.prepare_new_table();
 
             std::function<void(const std::filesystem::path&, LuaMadeSimple::Lua::Table&)> iterate_directory =
